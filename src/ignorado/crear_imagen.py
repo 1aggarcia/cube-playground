587b4126980583fa4ebe3e58a9a3e397aa0cfce7
Apolo Garcia-Herrera <77506622+1aggarcia@users.noreply.github.com>
2023-12-15 18:38:35 -0800

    Establish box and text drawing into png file

--- src/ignorado/crear_imagen.py
+++ src/ignorado/crear_imagen.py
@@ -1,20 +1,30 @@
-from PIL import Image
+import textwrap
+from PIL import Image, ImageFont, ImageDraw
 
 TAMANO = 100
 
 ROJO = (256, 000, 000)
-AZUL = (000, 256, 000)
-VERDE = (000, 000, 256)
+AZUL = (000, 000, 256)
+VERDE = (000, 256, 000)
 BLANCO = (256, 256, 256)
+NEGRO = (0, 0, 0)
+
+TEXTO = '39iu4r32948cru93rcu2h9384ruc98ewruc28989ur923807r2c38479293804728903'
+
+font = ImageFont.truetype("arial.ttf", 25)
 
 imagen = Image.new('RGB', (700, 500))
+draw = ImageDraw.Draw(imagen)
 
 imagen.paste(BLANCO, (0, 0, 700, 500))
 
-imagen.paste(VERDE, (TAMANO, TAMANO, TAMANO * 2, TAMANO * 2))
-imagen.paste(ROJO, (TAMANO * 2, TAMANO * 2, TAMANO * 3, TAMANO * 3))
-imagen.paste(AZUL, (TAMANO * 3, TAMANO * 3, TAMANO * 4, TAMANO * 4))
+# cuadrato 2x2
+draw.rectangle((20, 20, 40, 40), VERDE, NEGRO)
+draw.rectangle((20, 40, 40, 60), VERDE, NEGRO)
+draw.rectangle((40, 20, 60, 40), VERDE, NEGRO)
+draw.rectangle((40, 40, 60, 60), VERDE, NEGRO)
+
+draw.text((5, 5), "\n".join(textwrap.wrap(TEXTO, width=2)), NEGRO, font)
 
 imagen.save('./imagen.png')
 imagen.show()
-
